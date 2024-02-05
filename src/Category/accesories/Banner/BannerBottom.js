8b4ce323197dc73bf8d4ad41d313577fa30c28f5
MTahirSB <786mtsb@gmail.com>
2024-02-05 23:08:23 +0530

    category_4

---
+++ src/Category/accesories/Banner/BannerBottom.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MdLocalShipping } from "react-icons/md";
+import { CgRedo } from "react-icons/cg";
+
+const BannerBottom = () => {
+  return (
+    <div className="w-full border-b-[1px] py-4 border-b-gray-200 px-4">
+      <img src="https://images.bewakoof.com/uploads/grid/app/desktop-strip-rm--1--1704010481.jpg"></img>
+    </div>
+  );
+};
+
+export default BannerBottom;
